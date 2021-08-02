ab3a9a08b2ede54222650a1f1b0ba982697ffc36
김정환 <iz4blue@users.noreply.github.com>
2021-08-02 12:45:36 +0900

    Create install-minikube.sh

---
+++ devtools/manymany/install-minikube.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# https://minikube.sigs.k8s.io/docs/start/
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo dpkg -i minikube_latest_amd64.deb
