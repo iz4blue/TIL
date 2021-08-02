cce06d2c5f7ca6c8f16621ce79f32793cbdf5dc9
김정환 <iz4blue@users.noreply.github.com>
2021-08-02 12:49:13 +0900

    Update start-minikube.sh

--- devtools/manymany/start-minikube.sh
+++ devtools/manymany/start-minikube.sh
@@ -5,4 +5,8 @@
 sudo apt-get install -y conntrack
 
 # minikube cpu max
-sudo minikube start --vm-driver=none --memory=max --cpus=max
+sudo minikube start --vm-driver=none
+
+
+# https://minikube.sigs.k8s.io/docs/faq/#how-can-i-allocate-maximum-resources-to-minikube
+# sudo minikube start --memory=max --cpus=max
