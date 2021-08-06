82514ac78168ccef4ff5ea01c4413cb1b4fb9877
김정환 <iz4blue@users.noreply.github.com>
2021-08-06 12:38:56 +0900

    minikube 의 tmp 폴더로 설정되는 PVC 를 리부팅해도 유지되도록 설정 함

--- devtools/manymany/install-minikube.sh
+++ devtools/manymany/install-minikube.sh
@@ -4,3 +4,10 @@
 
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb
+
+# https://github.com/kubernetes/minikube/issues/7511
+mkdir -p /var/tmp/hostpath-provisioner
+mkdir /tmp/hostpath-provisioner
+sudo mount --bind /var/tmp/hostpath-provisioner /tmp/hostpath-provisioner
+
+echo '/var/tmp/hostpath-provisioner /tmp/hostpath-provisioner none defaults,bind 0 0' >> /etc/fstab
