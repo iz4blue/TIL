17234db798c5c9d02e0350030be87d7cb671a48f
김정환 <iz4blue@users.noreply.github.com>
2022-03-15 12:30:17 +0900

    Update install-kubectl.sh

--- devtools/manymany/install-kubectl.sh
+++ devtools/manymany/install-kubectl.sh
@@ -5,3 +5,12 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
+
+
+# ~/.bashrc 에 추가
+source /usr/share/bash-completion/bash_completion
+
+$ echo 'source <(kubectl completion bash)' >>~/.bashrc
+$ echo 'alias k=kubectl' >>~/.bashrc
+$ echo 'complete -F __start_kubectl k' >>~/.bashrc
+
