5b3de9f16573d5dc2c277d6c0c87d2ade5f53f01
김정환 <iz4blue@users.noreply.github.com>
2023-09-08 17:04:42 +0900

    Update install-docker.sh
    docker compose plugin 추가

--- devtools/manymany/install-docker.sh
+++ devtools/manymany/install-docker.sh
@@ -14,7 +14,7 @@ echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 
 sudo usermod -aG docker $USER
