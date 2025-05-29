a6c25d8d2021a460f3d9aa812ac032cf6fc2dfb1
김정환 <iz4blue@users.noreply.github.com>
2025-05-29 15:36:46 +0900

    Update start-minikube.sh

--- devtools/manymany/start-minikube.sh
+++ devtools/manymany/start-minikube.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 # https://krksap.tistory.com/1798
-
+'''
+Exiting due to GUEST_MISSING_CONNTRACK: Sorry, Kubernetes 1.33.1 requires conntrack to be installed in root's path
+'''
 sudo apt-get install -y conntrack
+
+# crictl install 
+- https://github.com/kubernetes-sigs/cri-tools?tab=readme-ov-file
+'''
+Exiting due to GUEST_MISSING_CONNTRACK: Sorry, Kubernetes 1.33.1 requires crictl to be installed in root's path
+'''
 
 # minikube cpu max
 sudo minikube start --vm-driver=none
