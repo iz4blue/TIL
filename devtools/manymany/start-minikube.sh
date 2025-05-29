16a97afb2b48fdc82ecbc9a1281eb544c4140b7c
김정환 <iz4blue@users.noreply.github.com>
2025-05-29 18:37:21 +0900

    Update start-minikube.sh

--- devtools/manymany/start-minikube.sh
+++ devtools/manymany/start-minikube.sh
@@ -12,6 +12,28 @@ sudo apt-get install -y conntrack
 Exiting due to GUEST_MISSING_CONNTRACK: Sorry, Kubernetes 1.33.1 requires crictl to be installed in root's path
 '''
 
+# aaa
+```
+Exiting due to NOT_FOUND_CRI_DOCKERD
+```
+- https://github.com/Mirantis/cri-dockerd/issues/361#issuecomment-2387142829
+
+# bbbb
+```
+Exiting due to HOST_JUJU_LOCK_PERMISSION: Failed to start host: boot lock: unable to open /tmp/juju-mkc8ab01ad3ea83211c505c81a7ee49a8e3ecb89: permission denied
+```
+
+sudo sysctl fs.protected_regular=0
+
+
+# cccc
+```
+Exiting due to NOT_FOUND_CNI_PLUGINS
+```
+- https://minikube.sigs.k8s.io/docs/faq/#how-do-i-install-containernetworking-plugins-for-none-driver
+
+
+
 # minikube cpu max
 sudo minikube start --vm-driver=none
 
